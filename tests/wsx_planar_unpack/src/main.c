d4d26701504d5ad2761c5981462203ca2f432afb
Adrian Siekierka <kontakt@asie.pl>
2025-06-15 12:05:38 +0200

    tests/wsx_planar_unpack: libws v2 update

--- tests/wsx_planar_unpack/src/main.c
+++ tests/wsx_planar_unpack/src/main.c
@@ -10,8 +10,9 @@
 #define SCREEN1 ((uint16_t*) 0x1800)
 
 static void draw_smileys_on_screen(int count) {
+	ws_display_set_screen1_address(0x1800);
+
 	// Clear screen 1.
-	outportb(IO_SCR_BASE, SCR1_BASE(0x1800));
 	memset(SCREEN1, 0, 0x800);
 
 	// Set tiles on screen.
@@ -27,51 +28,51 @@ static void wait_for_keypress(uint16_t mask) {
 
 void main(void) {
 	// Initialize display palette.
-	ws_display_set_shade_lut(SHADE_LUT_DEFAULT);
-	outportw(0x20, 0x7520);
+	ws_display_set_shade_lut(WS_DISPLAY_SHADE_LUT_DEFAULT);
+	outportw(WS_SCR_PAL_0_PORT, 0x7520);
 
 	// Unpack tiles.
-	memset(MEM_TILE(0), 0, 16);
+	memset(WS_TILE_MEM(0), 0, 16);
 	for (int i = 0; i < 3; i++) {
-		wsx_planar_unpack(MEM_TILE(i + 1), 8, smiley, WSX_PLANAR_UNPACK_1BPP_TO_2BPP_ZERO(i));
+		wsx_planar_unpack(WS_TILE_MEM(i + 1), 8, smiley, WSX_PLANAR_UNPACK_1BPP_TO_2BPP_ZERO(i));
 	}
 	draw_smileys_on_screen(3);
 
 	// Enable display.
-	outportw(IO_DISPLAY_CTRL, DISPLAY_SCR1_ENABLE);
+	outportw(WS_DISPLAY_CTRL_PORT, WS_DISPLAY_CTRL_SCR1_ENABLE);
 
-	wait_for_keypress(KEY_A);
+	wait_for_keypress(WS_KEY_A);
 
-	outportw(IO_DISPLAY_CTRL, 0);
+	outportw(WS_DISPLAY_CTRL_PORT, 0);
 
 	for (int i = 0; i < 16; i++) {
-		wsx_planar_unpack(MEM_TILE(i), 8, smiley, WSX_PLANAR_UNPACK_1BPP_TO_2BPP((i >> 2), (i & 3)));
+		wsx_planar_unpack(WS_TILE_MEM(i), 8, smiley, WSX_PLANAR_UNPACK_1BPP_TO_2BPP((i >> 2), (i & 3)));
 	}
 	draw_smileys_on_screen(15);
 
-	outportw(IO_DISPLAY_CTRL, DISPLAY_SCR1_ENABLE);
+	outportw(WS_DISPLAY_CTRL_PORT, WS_DISPLAY_CTRL_SCR1_ENABLE);
 
-	wait_for_keypress(KEY_A);
+	wait_for_keypress(WS_KEY_A);
 
-	if (ws_system_is_color()) {
-		outportw(IO_DISPLAY_CTRL, 0);
-		ws_mode_set(WS_MODE_COLOR_4BPP);
+	if (ws_system_is_color_model()) {
+		outportw(WS_DISPLAY_CTRL_PORT, 0);
+		ws_system_set_mode(WS_MODE_COLOR_4BPP);
 
 		// Initialize 4BPP display mode.
 		for (int i = 0; i < 16; i++) {
-			MEM_COLOR_PALETTE(0)[i] = (15 - i) * 0x111;
+			WS_DISPLAY_COLOR_MEM(0)[i] = (15 - i) * 0x111;
 		}
 
 		// Unpack tiles.
-		memset(MEM_TILE_4BPP(0), 0, 32);
+		memset(WS_TILE_4BPP_MEM(0), 0, 32);
 		for (int i = 0; i < 15; i++) {
-			wsx_planar_unpack(MEM_TILE_4BPP(i + 1), 8, smiley, WSX_PLANAR_UNPACK_1BPP_TO_4BPP_ZERO(i));
+			wsx_planar_unpack(WS_TILE_4BPP_MEM(i + 1), 8, smiley, WSX_PLANAR_UNPACK_1BPP_TO_4BPP_ZERO(i));
 		}
 
 		draw_smileys_on_screen(15);
 
 		// Enable display.
-		outportw(IO_DISPLAY_CTRL, DISPLAY_SCR1_ENABLE);
+		outportw(WS_DISPLAY_CTRL_PORT, WS_DISPLAY_CTRL_SCR1_ENABLE);
 	}
 
 	while(1);
