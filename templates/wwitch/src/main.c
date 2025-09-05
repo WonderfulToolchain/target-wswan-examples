655f3f9263f2b28dae8d24d4fc972bfb46ff73aa
Adrian Siekierka <kontakt@asie.pl>
2025-09-05 17:27:49 +0200

    templates: add usage

--- templates/wwitch/src/main.c
+++ templates/wwitch/src/main.c
@@ -6,6 +6,6 @@
 
 void main(int argc, char** argv) {
 	text_screen_init();
-	text_put_string((LCD_CHAR_WIDTH - 13) / 2, LCD_CHAR_HEIGHT / 2, "Hello, World!");
+	text_put_string(0, 0, "Hello, World!");
 	while (key_wait() != KEY_START);
 }
